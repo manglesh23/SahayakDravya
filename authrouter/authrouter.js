c89ec0a45a7a13cff6916963e77df2d554ee303d
manglesh23 <mangleshyadav2@gmail.com>
2024-07-01 13:14:16 +0530

    documentation

--- authrouter/authrouter.js
+++ authrouter/authrouter.js
@@ -1,20 +1,38 @@
+/* -------------------------------------------------------------------------- */
+/*                                Dependencies                                */
+/* -------------------------------------------------------------------------- */ 
 const express=require('express');
 const router=express.Router();
 
 const {home}=require('../controllers/home');
 // const{login}=require('../controllers/login');
 const getUser=require('../controllers/user');
-const {createCenter, findCenter}=require('../controllers/center')
+
+const {createCenter, findCenter}=require('../controllers/center');
+
 const {verifyToken}=require('../middleware/verify-token');
+
 const { addMember } = require('../controllers/member');
+
 const { uploadDataFile } = require('../controllers/uploadDataFile');
+
 const { uploadfileUsingMulter } = require('../middleware/multer');
+
 const { convertFileCSVToJSON } = require('../controllers/convertCSVtoJSON');
+
 const { uploadToAWS } = require('../controllers/uploadToAWS');
+
 // const { uploadmulterS3 } = require('../middleware/multer_s3');
+
 const {uploadTOAWSviaMulters3}=require('../controllers/uploadAWSmulterS3');
+
 const { uploadMulterS3 } = require('../middleware/multer_s3');
+
 const { uploadmiddlewaremulters3 } = require('../controllers/multerS3middleware');
+
+/* -------------------------------------------------------------------------- */
+/*                                Dependencies                                */
+/* -------------------------------------------------------------------------- */              
 
 router.route("/").get(home);
 // router.route("/login").get(login);
