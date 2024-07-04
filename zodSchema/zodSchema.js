27a605f723fbe2688f8e5466492448d751d9f7f6
manglesh23 <mangleshyadav2@gmail.com>
2024-07-04 18:57:54 +0530

    mobile  number validation

--- zodSchema/zodSchema.js
+++ zodSchema/zodSchema.js
@@ -3,7 +3,7 @@ const { z } = require('zod');
 const userSchema = z.object({
   name: z.string().min(3, 'Name is required'),
   email: z.string().email('Invalid email address'),
-  mobilenumber: z.string().min(10,'Must be of length 10'),
+  mobilenumber: z.string().length(10,'Must be of length 10'),
 });
 
 module.exports = { userSchema };
