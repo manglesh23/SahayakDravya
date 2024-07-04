9e46e6f6a5dc67210630597774c0586fe8106ec7
manglesh23 <mangleshyadav2@gmail.com>
2024-07-05 00:28:24 +0530

    zod validation

--- middleware/zodvalidator.js
+++ middleware/zodvalidator.js
@@ -3,7 +3,7 @@ const { z } = require('zod');
 const validate = (schema) => (req, res, next) => {
   const result = schema.safeParse(req.body);
   if (!result.success) {
-    return res.status(400).json(result.error.errors);
+    return res.status(400).json({msg:result.error.errors});
   }
   req.validatedData = result.data;
   next();
